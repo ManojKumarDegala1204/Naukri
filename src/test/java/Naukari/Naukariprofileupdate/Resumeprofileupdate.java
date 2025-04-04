efe4fcaf2d10493a21fb1e4a30972ccbb1bfbe12
ManojKumar Degala <manojkumardegala1204@gmail.com>
2025-04-04 19:47:35 +0530

    plugins modified

--- src/test/java/Naukari/Naukariprofileupdate/Resumeprofileupdate.java
+++ src/test/java/Naukari/Naukariprofileupdate/Resumeprofileupdate.java
@@ -1,10 +1,9 @@
 package Naukari.Naukariprofileupdate;
 
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 import org.testng.Reporter;
-import org.testng.annotations.Test;
 import java.time.Duration;
-
-import org.testng.annotations.Test;
 
 public class Resumeprofileupdate extends Baseclass {
 	//update the resume headline
